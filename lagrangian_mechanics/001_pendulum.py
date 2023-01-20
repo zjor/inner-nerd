d895303b2f57c4f837fb0d22999165103d06fe40
Sergey Royz <zjor.se@gmail.com>
2023-01-20 12:52:11 +0100

    simple pendulum :: scene :: part 1

--- lagrangian_mechanics/001_pendulum.py
+++ lagrangian_mechanics/001_pendulum.py
@@ -51,8 +51,72 @@ thetas = solution[:, 0]
 xs = L * np.sin(thetas)
 ys = -L * np.cos(thetas)
 
+primary_params = {
+    "stroke_color": GREEN_B,
+    "fill_opacity": 1,
+    "fill_color": GREEN_B
+}
+
+secondary_params = {
+    "stroke_color": BLUE_C,
+    "stroke_width": 2.0
+}
+
+drawing_symbols_params = {
+    "color": RED_C,
+    "font_size": 36
+}
+
+
+class SimplePendulum:
+    def __init__(self, initial_angle: float = PI / 6, l: float = 4.0, r: float = 0.3):
+        self.rod_start = np.array((0, l / 2, 0))
+
+        _th = initial_angle
+        _th_ = _th + 0.15
+        self.rod_end = np.array((-l * np.sin(_th), l / 2 - l * np.cos(_th), 0))
+        self.g_rod = Line(start=self.rod_start, end=self.rod_end, **primary_params)
+        self.g_mass = Circle(radius=r, **primary_params).move_to(self.rod_end)
+
+        self.g_ceiling = Line(start=self.rod_start + 0.5 * LEFT, end=self.rod_start + 0.5 * RIGHT, **primary_params)
+        self.g_symmetry_line = DashedVMobject(
+            Line(start=(0, l / 2 + 0.2, 0), end=(0, -(l / 2 + 0.2), 0), **secondary_params),
+            num_dashes=12
+        )
+        self.g_angle_arc = Arc(radius=0.5, start_angle=-PI / 2, angle=-_th, arc_center=self.rod_start,
+                               **secondary_params)
+        self.g_trajectory = DashedVMobject(
+            Arc(radius=l, start_angle=-_th_ - PI / 2, angle=2 * _th_, arc_center=self.rod_start, **secondary_params),
+            num_dashes=12
+        )
+        self.g_trajectory.set_z_index(self.g_mass.z_index - 1)
+        self.g_text_theta = MathTex(r"\theta", **drawing_symbols_params).next_to(self.g_angle_arc, RIGHT)
+        self.g_text_l = MathTex(r"L", **drawing_symbols_params) \
+            .move_to((self.rod_start + self.rod_end) / 2 + LEFT / 2)
+        self.g_text_m = MathTex(r"m", **drawing_symbols_params).next_to(self.g_mass, DOWN)
+
+        self.g_symbols = VGroup(*[
+            self.g_text_theta,
+            self.g_text_l,
+            self.g_text_m
+        ])
+
+        self.g_all = VGroup(*[
+            self.g_rod,
+            self.g_mass,
+            self.g_ceiling,
+            self.g_symmetry_line,
+            self.g_angle_arc,
+            self.g_trajectory,
+            *self.g_symbols
+        ])
+
 
 class Scenario(MovingCameraScene):
+
+    def __init__(self):
+        super().__init__()
+        self.pendulum = SimplePendulum()
 
     def play_intro(self):
         _l = 2.0
@@ -60,17 +124,6 @@ class Scenario(MovingCameraScene):
         _th_ = _th + 0.3
         _origin = (0, 1, 0)
         _end = (-_l * np.sin(_th), 1.0 - _l * np.cos(_th), 0)
-
-        primary_params = {
-            "stroke_color": GREEN_B,
-            "fill_opacity": 1,
-            "fill_color": GREEN_B
-        }
-
-        secondary_params = {
-            "stroke_color": BLUE_A,
-            "stroke_width": 2.0
-        }
 
         group = VGroup()
         group.add(*[
@@ -108,8 +161,39 @@ class Scenario(MovingCameraScene):
         self.play(Write(text_2))
         self.wait(5)
 
+    def play_scenario(self):
+        p = self.pendulum
+        self.play(Create(p.g_symmetry_line), run_time=0.5)
+        self.play(
+            FadeIn(*[
+                p.g_ceiling,
+                p.g_mass,
+                p.g_rod
+            ]),
+            run_time=0.5
+        )
+        self.play(
+            FadeIn(*[
+                p.g_angle_arc,
+                p.g_trajectory
+            ]),
+            run_time=0.5
+        )
+        self.play(
+            FadeIn(*[
+                p.g_text_theta,
+                p.g_text_m,
+                p.g_text_l
+            ]),
+            run_time=0.5
+        )
+
+        self.play(p.g_all.animate.scale(0.75).shift(4.0 * RIGHT))
+        self.wait(0.5)
+
     def construct(self):
-        self.play_intro()
+        # self.play_intro()
+        self.play_scenario()
         return
         time = ValueTracker(0)
 
