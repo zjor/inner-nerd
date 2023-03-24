f84ab275b7a95817553d6ecec8a04ca6f6c7ff16
Sergey Royz <zjor.se@gmail.com>
2023-03-24 12:11:53 +0100

    circle intersections

--- sacral_geometry/circles_intersection.py
+++ sacral_geometry/circles_intersection.py
@@ -3,6 +3,7 @@ from typing import List
 from math import sqrt
 from manim import *
 
+
 @dataclass
 class Point:
     x: float
@@ -11,28 +12,42 @@ class Point:
     def to_array(self):
         return [self.x, self.y, 0]
 
+    def middle(self, that: Point):
+        return Point((self.x + that.x) / 2, (self.y + that.y) / 2)
 
-def find_intersection(a: Point, b: Point, r: float) -> List[Point]:
-    # TODO: handle dist(a, b) > 2 * r
-    o = Point((a.x + b.x) / 2, (a.y + b.y) / 2)
-    l2 = (o.x - a.x) ** 2 + (o.y - a.y) ** 2
+    def dist2(self, that: Point):
+        return (self.x - that.x) ** 2 + (self.y - that.y) ** 2
+
+
+def find_intersection(a: Point, b: Point, r: float) -> List[Point]:    
+    o = a.middle(b)
+    l2 = o.dist2(a)
+
+    if l2 > r ** 2:
+        return []
 
     if abs(a.x - b.x) > abs(a.y - b.y):
-      alpha = (o.y - a.y) / (o.x - a.x) 
-    else:
-      alpha = (o.x - a.x) / (o.y - a.y) 
+        alpha = (o.y - a.y) / (o.x - a.x)
+        beta = sqrt((r ** 2 - l2) / (1 + alpha ** 2))
+        return [
+            Point(o.x - alpha * beta, o.y + beta),
+            Point(o.x + alpha * beta, o.y - beta),
+        ]
 
-    beta = sqrt((r ** 2 - l2) / (1 + alpha ** 2))        
-    return [
-        Point(o.x - alpha * beta, o.y + beta),
-        Point(o.x + alpha * beta, o.y - beta),
-    ]
+    else:
+        alpha = (o.x - a.x) / (o.y - a.y)
+        beta = sqrt((r ** 2 - l2) / (1 + alpha ** 2))
+        return [
+            Point(o.x + beta, o.y - alpha * beta),
+            Point(o.x - beta, o.y + alpha * beta),
+        ]
     
+
 
 class Scenario(Scene):
     def construct(self):
-        a = Point(-0.5, -0.1)
-        b = Point(0.5, 0.2)
+        a = Point(-0.3, 0.0)
+        b = Point(0.3, 0.0)
         r = 1.5
         c1 = Circle(radius=r, color=RED).move_to(a.to_array())
         c2 = Circle(radius=r, color=BLUE).move_to(b.to_array())
